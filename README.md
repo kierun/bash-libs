77ad8033336b41b3b9167ed13f6af0c9fb5d302a
Yann Golanski <yann@kierun.org>
2017-04-21 14:47:04 +0100

    Added wallpaper rotation script.

--- README.md
+++ README.md
@@ -4,8 +4,28 @@ A collections of really useful[⸮](https://en.wikipedia.org/wiki/Irony_mark) ba
 
 ## What is in there?
 
-+ **logging.bash** This is an over-engineered bash script template with 256 colour logging…
+### logging.bash
+
+This is an over-engineered bash script template with 256 colour logging…
+
+### sleeper.bash
+
+To be documented…
+
+### wallpaper_rotation.bash
+
+You should have one top level directory, let's call this `~/wallpapers`.  Your
+wallpapers need to be split into several sub directories.
+
+A preferred directory (`~/wallpapers/preferred`) which contains the best of
+the best images. Those are the ones that you will see most often in the
+rotation.
+
+The rest should be put in directories by date (`~/wallpapers/2016`,
+``~/wallpapers/2015`) when you got the wallpaper. Assuming that you have a
+more or less linear progression (say `2014` → `2015` → `2016` → `2017`) then
+the more recent directory will get a higher priority.
 
 ## Notes.
 
-*Criticism, bug reports, and improvements are most welcome! ☺*
+**Criticism, bug reports, and improvements are most welcome! ☺**
---
+++ wallpaper_rotation.bash
@@ -0,0 +1,90 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*-
+################################################################################
+## Configuration!
+
+# Where are your wallpapers kept?
+wallpapers=~/wallpapers
+
+# How often you want your wallpaper to change? Passed to sleep…
+frequency="10m"
+
+# What program, with options, do you use to set your wallpaper?
+setter="`which feh` --bg-scale"
+
+# When do you want the rotating to pause. Set this above 2400 for always.
+pause_time="2500"
+
+# How long do we wish this to pause for?
+pause_duration="12 hours 40 minutes"
+
+# What are the days you do not wish to work on. An empty array is all the time.
+skip_today=()
+
+################################################################################
+## ⚠  Ignore things below this line…
+
+# Function that returns 0 if and element is in the array.
+function containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+# Do the hard work…
+while true; do
+
+    # Get all the yearly directories.
+    shopt -s nullglob
+    yeardirs=(${wallpapers}/2*/) 
+    shopt -u nullglob
+
+    # Add as many instances of preferred as there are yearly directories.
+    for i in $(eval echo "{0..${#yeardirs[@]}}")
+    do
+        dirs+=(${wallpapers}/preferred)
+    done
+
+    # Add each yearly directory to the main list, starting with the earlier.
+    END=$((${#yeardirs[@]} - 1))
+    for i in $(eval echo "{0..$END}")
+    do
+        for j in $(eval echo "{0..$i}")
+        do
+            dirs+=(${yeardirs[$i]})
+        done
+    done
+
+    # Now, get all the files and shuffle them!
+    array+=($(/usr/bin/find "${dirs[@]}" -type f | shuf))
+
+    # Loop through the array, set as back drop, and sleep till next time.
+    for img in "${array[@]}"; do
+        if containsElement `date +'%a'` "${skip_today[@]}"; then
+            # Do not run this day.
+            echo "This is the weekend"
+            current_epoch=$(date +%s)
+            target_epoch=$(date -d "tomorrow" +%s)
+            sleep_seconds=$(( $target_epoch - $current_epoch ))
+            sleep $sleep_seconds
+        else
+            # We can run, but when?
+            now=`date +'%H%M'`
+            if [ $now -ge $pause_time ];
+            then
+                # This is outside the end time!
+                echo "It's late, go home!"
+                current_epoch=$(date +%s)
+                target_epoch=$(date -d "${pause_duration}" +%s)
+                sleep_seconds=$(( $target_epoch - $current_epoch ))
+                sleep $sleep_seconds
+            else
+                # We can set the wallpaper.
+                echo $img
+                $setter $img
+                sleep $frequency
+            fi
+        fi
+    done
+done
+# eof
