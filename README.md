9fb2a1ab4d6e23afd87c506fb689719642fd671b
Dr Yann Golanski <github@kierun.org>
2023-05-23 17:40:59 +0100

    feat(wallpaper): updates and cleanup

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# bash-libs
+# bash libs
 
-A collections of really useful[⸮](https://en.wikipedia.org/wiki/Irony_mark) bash scripts that you can source into your own script to make your script more niffty.
+A collections of really useful[⸮](https://en.wikipedia.org/wiki/Irony_mark) bash
+scripts that you can source into your own script to make your script more nifty.
 
 ## What is in there?
 
-### logging.bash
+### `logging.bash`
 
 This is an over-engineered bash script template with 256 colour logging…
 
@@ -12,20 +13,17 @@ This is an over-engineered bash script template with 256 colour logging…
 
 To be documented…
 
-### wallpaper_rotation.bash
+### `wallpaper_rotation.bash`
 
-You should have one top level directory, let's call this `~/wallpapers`.  Your
+You should have one top level directory, let's call this `~/wallpapers`. Your
 wallpapers need to be split into several sub directories.
 
-A preferred directory (`~/wallpapers/preferred`) which contains the best of
-the best images. Those are the ones that you will see most often in the
-rotation.
+A preferred directory (`~/wallpapers/Preferred`) which contains the best of the
+best images. Those are the ones that you will see most often in the rotation.
 
-The rest should be put in directories by date (`~/wallpapers/2016`,
-``~/wallpapers/2015`) when you got the wallpaper. Assuming that you have a
-more or less linear progression (say `2014` → `2015` → `2016` → `2017`) then
-the more recent directory will get a higher priority.
+The rest should be put in directories with a capital letter. Something like
+`Witcher` or whatever. They all have equal weight.
 
-## Notes.
+## Notes
 
 **Criticism, bug reports, and improvements are most welcome! ☺**
--- wallpaper_rotation.bash
+++ wallpaper_rotation.bash
@@ -10,30 +10,19 @@ wallpapers=~/wallpapers
 frequency="10m"
 
 # What program, with options, do you use to set your wallpaper?
-setter="`which feh` --bg-scale"
+setter="$(which feh) --bg-scale"
 
 # When do you want the rotating to pause. Set this above 2400 for always.
-pause_time="1600"
+pause_time="2401"
 
 # How long do we wish this to pause for?
-pause_duration="15 hours"
+pause_duration="1 hours"
 
 # What are the days you do not wish to work on. An empty array is all the time.
-skip_today=("Sat" "Sun")
+skip_today=()
 
 ################################################################################
 ## ⚠  Ignore things below this line…
-
-function __wallpaper_lib_help( ) {
-    cat <<- EOF
-Calling: . <path>/wallpaper.bash
-
-This file needs a bit of work to make it an includable lib, rather than a
-standalone process.
-
-EOF
-    exit 0
-}
 
 # Function that returns 0 if and element is in the array.
 function containsElement () {
@@ -41,65 +30,65 @@ function containsElement () {
   for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
   return 1
 }
+
+# Function that returns 1 if we are not running.
 function runToday() {
     local d
     d=$(date +%a)
-    for s in ${skip_today[@]}; do
-        if [ $s == $d ]; then
-            return 0
+    for s in "${skip_today[@]}"; do
+        if [ "$s" == "$d" ]; then
+            return 1
         fi
     done
-    return 1
+    return 0
 }
 
 # Do the hard work…
 while true; do
 
-    # Get all the yearly directories.
+    # Get all the old directories.
     shopt -s nullglob
-    yeardirs=(${wallpapers}/2*/) 
+    olddirs=("${wallpapers}"/[2A-Z]*/)
     shopt -u nullglob
 
-    # Add as many instances of preferred as there are yearly directories.
-    for i in $(eval echo "{0..${#yeardirs[@]}}")
+    # Add as many instances of preferred as there are old directories.
+    for i in $(eval echo "{0..${#olddirs[@]}}")
     do
-        dirs+=(${wallpapers}/preferred)
+        dirs+=("${wallpapers}"/Preferred)
     done
 
-    # Add each yearly directory to the main list, starting with the earlier.
-    END=$((${#yeardirs[@]} - 1))
+    # Add each old directory to the main list, starting with the earlier.
+    END=$((${#olddirs[@]} - 1))
     for i in $(eval echo "{0..$END}")
     do
-        for j in $(eval echo "{0..$i}")
+        for _ in $(eval echo "{0..$i}")
         do
-            dirs+=(${yeardirs[$i]})
+            dirs+=("${olddirs[$i]}")
         done
     done
 
     # Now, get all the files and shuffle them!
-    array+=($(/usr/bin/find "${dirs[@]}" -type f | shuf))
+    mapfile -t array < <(find "${dirs[@]}" -type f | shuf)
 
     # Loop through the array, set as back drop, and sleep till next time.
     COUNTER=0
     for img in "${array[@]}"; do
-        runToday
-        if [ $? -gt 0 ]; then
-        #if containsElement `date +'%a'` "${skip_today[@]}"; then
+        if runToday; then
             # We can run, but when?
-            now=`date +'%H%M'`
-            if [ $now -ge $pause_time ];
+            now=$(date +'%H%M')
+            if [ "$now" -ge "$pause_time" ];
             then
                 # This is outside the end time!
                 echo "It's late, go home!"
                 current_epoch=$(date +%s)
                 target_epoch=$(date -d "${pause_duration}" +%s)
-                sleep_seconds=$(( $target_epoch - $current_epoch ))
+                sleep_seconds=$(( target_epoch - current_epoch ))
                 sleep $sleep_seconds
             else
                 # We can set the wallpaper.
-                let COUNTER=COUNTER+1
-                echo "`date` ${COUNTER}/${#array[@]}: $img"
-                $setter $img
+                (( COUNTER=COUNTER+1 ))
+                echo "$(date) ${COUNTER}/${#array[@]}: $img"
+                $setter "$img"
                 sleep $frequency
             fi
         else
@@ -107,16 +96,8 @@ while true; do
             echo "This is the weekend"
             current_epoch=$(date +%s)
             target_epoch=$(date -d "tomorrow" +%s)
-            sleep_seconds=$(( $target_epoch - $current_epoch ))
+            sleep_seconds=$(( target_epoch - current_epoch ))
             sleep $sleep_seconds
         fi
     done
 done
-# eof
-
-this=$(realpath $BASH_SOURCE)
-orig=$(realpath $0)
-
-if [ $this == $orig ]; then
-    __wallpaper_lib_help
-fi
