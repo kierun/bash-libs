0900ec9e5bcdc44e35358fa23c744e6e69fe2099
Dr Yann Golanski <github@kierun.org>
2023-05-24 09:06:51 +0100

    fix(GHA): shellcheck fixes

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 TARGET=${1:-$HOME/bin/lib}
 
 # get our sources
-src=$(realpath $0)
-dir=$(dirname $src)
-cd $dir || exit 1
+src=$(realpath "$0")
+dir=$(dirname "$src")
+cd "$dir" || exit 1
 
-files=($(/bin/ls -1f | grep 'bash$' | fgrep -v install.bash))
+# files=($(/bin/ls -1f | grep 'bash$' | grep -Fv install.bash))
+mapfile -t files < <(/bin/ls -1f | grep 'bash$' | grep -Fv install.bash)
 
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 
-for f in ${files[@]}; do
-    install -m755 -pv $f $TARGET/$f
+for f in "${files[@]}"; do
+    install -m755 -pv "$f" "$TARGET/$f"
 done
