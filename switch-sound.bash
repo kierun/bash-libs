0a8c9d43f1c04477c8ff924fb7c46d4b50d532d4
Dr Yann Golanski <github@kierun.org>
2023-05-24 12:53:51 +0100

    fix(pre-commit): editorconfig added

--- switch-sound.bash
+++ switch-sound.bash
@@ -2,20 +2,20 @@
 
 while getopts "h?sp" opt; do
     case "$opt" in
-        h | \?)
-            echo "Usage: $0 [-c] [-f] [-h]"
-            echo "-s for speakers"
-            echo "-h for headphones"
-            echo "-h for help"
-            exit 0
-            ;;
-        s)
-            echo "Switching to speakers"
-            pactl set-sink-port 0 analog-output-lineout
-            ;;
-        p)
-            echo "Switching to headphones!"
-            pactl set-sink-port 0 analog-output-headphones
-            ;;
+    h | \?)
+        echo "Usage: $0 [-c] [-f] [-h]"
+        echo "-s for speakers"
+        echo "-h for headphones"
+        echo "-h for help"
+        exit 0
+        ;;
+    s)
+        echo "Switching to speakers"
+        pactl set-sink-port 0 analog-output-lineout
+        ;;
+    p)
+        echo "Switching to headphones!"
+        pactl set-sink-port 0 analog-output-headphones
+        ;;
     esac
 done
