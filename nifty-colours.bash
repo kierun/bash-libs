1d712731c5a11e720677079497a26430f09b1f63
Dr Yann Golanski <github@kierun.org>
2023-05-24 12:40:01 +0100

    fix(GHA): shfmt

--- nifty-colours.bash
+++ nifty-colours.bash
@@ -10,22 +10,22 @@ success=$(tput setaf 2)   # normal green.
 warning=$(tput setaf 214) # orange.
 info=$(tput setaf 99)     # purple.
 header=$(tput setaf 69)   # light blue.
-debug=$(tput setaf 240) # grey.
+debug=$(tput setaf 240)   # grey.
 reset=$(tput sgr0)
 
 header() {
-    offset=$2
-    title="___/ $1 \\"
-    len=${#title}
-    cols=$(tput cols)
-    echo -en "${header}"
-    echo -en "    "
-    for ((i = 4; i < len - offset; i++)); do printf "_"; done
-    echo
-    echo -en "$title"
-    for ((i = len; i < cols; i++)); do printf "_"; done
-    echo
-    echo -en "${reset}"
+	offset=$2
+	title="___/ $1 \\"
+	len=${#title}
+	cols=$(tput cols)
+	echo -en "${header}"
+	echo -en "    "
+	for ((i = 4; i < len - offset; i++)); do printf "_"; done
+	echo
+	echo -en "$title"
+	for ((i = len; i < cols; i++)); do printf "_"; done
+	echo
+	echo -en "${reset}"
 }
 
 header "\uebc7  Nifty colours and header…" 6
