2923e30459bed673c4c42b330824a426d1f61e21
Dr Yann Golanski <github@kierun.org>
2025-03-04 15:13:39 +0000

    fix(mp4 to gif): Handles more files at once

--- convert_mp4_gif.bash
+++ convert_mp4_gif.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-ffmpeg \
-    -i "$1" \
-    -r 24 \
-    -vf scale=1024:-1 \
-    "$2"
+for file in "$@"; do
+    clean_file="$file" # Probably not worth trying to fix the file name…
+    filename=$(basename -- "$clean_file")
+    dst="$HOME/Pictures/gifs"
+    # extension="${filename##*.}"
+    filename="${filename%.*}"
+    echo "Doing $clean_file →  $dst/$filename.gif"
+    ffmpeg -i "$clean_file" -r 24 -vf scale=1024:-1 -v warning "$dst/$filename.gif"
+    mv -i "$clean_file" "$dst"
+done
